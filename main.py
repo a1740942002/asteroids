78fbb44fdff4079031002933622d977fd36698be
a1740942002 <a1740942002@me.com>
2024-11-10 13:17:12 +0800

    feat: Improve code readability and performance
    - Refactor code to improve readability and maintainability
    - Improve performance by adding framerate limiting to 60 FPS in the game loop
    - Update syntax to use `from constants import *` for importing all constants

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 
 
 def main():
-  pygame.init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-  while True:
-    for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        return
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-    pygame.Surface.fill(screen, pygame.Color("black"))
-    pygame.display.flip()
+        screen.fill("black")
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
-  main()
+    main()
